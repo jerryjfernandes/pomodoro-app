b4a2898a6a78eee420217e79ff52947645b495cf
Jerry Jones <jerry.jones@cubos.io>
2023-10-18 14:22:30 -0300

    feat: add components header and default layout

---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  max-width: 60rem;
+  height: calc(100vh);
+  margin: 5rem auto;
+  padding: 2.5rem;
+
+  background: ${(props) => props.theme['gray-800']};
+  border-radius: 8px;
+
+  display: flex;
+  flex-direction: column;
+`
